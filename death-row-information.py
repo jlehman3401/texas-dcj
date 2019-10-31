77e14547e05cc86f2b7314d9d625b583c787b7a8
Jonathan Lehman <37116484+jlehman3401@users.noreply.github.com>
2019-10-31 01:02:19 -0400

    adds file writing line

--- death-row-information.py
+++ death-row-information.py
@@ -1,30 +1,14 @@
 from bs4 import BeautifulSoup
 import requests
 
-url = 'https://www.tdcj.texas.gov/death_row/dr_executed_offenders.html'
+url = 'https://www.tdcj.texas.gov/death_row/dr_executed_texas.html'
 
 response = requests.get(url)
 
 soup = BeautifulSoup(response.text, 'html.parser')
 
 
-
-td_tags = soup.find_all('td')
-
-
-
-headers = [ 'Execution_id',
-            'Offender_Link',
-            'Last_Statement',
-            'Last_Name',
-            'First_Name',
-            'TDCJ_Number',
-            'Age',
-            'Date',
-            'Race',
-            'County']
-
-offenders = dict()
+texas = dict()
 
 # getting table values with offender and 
 td_tags = soup.find_all('td')
@@ -34,15 +18,15 @@ td_items = list()
 for element in td_tags:
     td_items.append(element.text)
 
-offenders['Execution_id'] = td_items[0:len(td_items):10]
+texas['Execution_id'] = td_items[0:len(td_items):10]
 
-offenders['Last_Name'] = td_items[3:len(td_items):10]
-offenders['First_Name'] = td_items[4:len(td_items):10]
-offenders['TDCJ_Number'] = td_items[5:len(td_items):10]
-offenders['Age'] = td_items[6:len(td_items):10]
-offenders['Date'] = td_items[7:len(td_items):10]
-offenders['Race'] = td_items[8:len(td_items):10]
-offenders['County'] = td_items[9:len(td_items):10]
+texas['Last_Name'] = td_items[3:len(td_items):10]
+texas['First_Name'] = td_items[4:len(td_items):10]
+texas['TDCJ_Number'] = td_items[5:len(td_items):10]
+texas['Age'] = td_items[6:len(td_items):10]
+texas['Date'] = td_items[7:len(td_items):10]
+texas['Race'] = td_items[8:len(td_items):10]
+texas['County'] = td_items[9:len(td_items):10]
 
 
 # get links from a tags
@@ -54,10 +38,14 @@ for a in a_tags:
 
 links = all_links[26:1156]
 
-offenders['Offender_Link'] = links[:len(links):2]
-offenders['Last_Statement'] = links[1:len(links):2]
+texas['Offender_Link'] = links[:len(links):2]
+texas['Last_Statement'] = links[1:len(links):2]
+texas_len = len(texas['Offender_Link'])
 
 
+filename = 'texas-dcj-offender-info.txt'
 
-
+with open(filename, "w") as f:
+    for i in range(len(texas_len)):
+        f.write(texas['Execution_id'][i] + ',' + texas['Last_Statement']+ '\n')
 
