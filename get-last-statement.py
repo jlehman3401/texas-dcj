d2f6b5b640ad8c1896224a50a5914f75d38a655d
jlehman3401 <lehman.255@gmail.com>
2019-12-09 00:24:22 -0500

    adds tag parsing; dataframe file writer

--- get-last-statement.py
+++ get-last-statement.py
@@ -17,17 +17,30 @@ for i in range(len(texas_df)):
 
 	p_tags = soup.find_all('p')
 
+
+	p_tag_list = list()
+	headers = list()
+	values = list()
+
+	for p in p_tags:
+   		p_tag_list.append(p.text)
+
+   	for item in p_tag_list:
+   		if p_tag_list.index(item) % 2 == 0:
+   			headers.append(item)
+    	if p_tag_list.index(item) % 2 == 1:
+        	values.append(item)
+
+    offender_dict = dict(zip(headers, values))
+
+    offender_df = pd.DataFrame(offender_dict)
+
 	ex_id = texas_df.index[i]
 	last_name = texas_df.iloc[i][0]
 	first_name = texas_df.iloc[i][1]
 
-	filename = f'{ex_id}-{last_name}-{first_name}-last-statement.txt'
-
-	with open(filename, 'w') as f:
-		for p in p_tags:
-			f.write(p.text)
-	f.close()
+	offender_df.to_csv(f'{ex_id}-{last_name}-{first_name}-last-statement.csv', header=True, index=False)
 
 	print(f'offender {ex_id} statement gathered')
 
-	time.sleep(1)
+	time.sleep(2)
