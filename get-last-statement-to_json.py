d4f2fc9d3890ff26dcfa6caf5fa7cacddc4a5057
jlehman3401 <lehman.255@gmail.com>
2019-12-14 00:01:06 -0500

    adds formating to json

--- get-last-statement-to_json.py
+++ get-last-statement-to_json.py
@@ -9,8 +9,8 @@ response = requests.get(url)
 
 soup = BeautifulSoup(response.text, 'html.parser')
 
-# parsing html for p-tags and establishing dictionary
-# need to create handling for unicode / ascii characters
+p_tags = soup.find_all('p')
+
 p_tag_list = list()
 headers = list()
 values = list()
@@ -21,15 +21,15 @@ for p in p_tags:
     
 name_num = p_tag_list[3].split('#')
 
-headers = [p_tag_list[0]] + [p_tag_list[0]] + ['TDCJ_num:'] + [p_tag_list[4]]
+headers = [p_tag_list[0].strip()] + [p_tag_list[2].strip()] + ['TDCJ_num:'] + [p_tag_list[4]]
 
 values = [p_tag_list[1]] + [name_num[0].strip()] + [name_num[1]] + p_tag_list[5:]
 
 
 offender_dict = dict(zip(headers, values))
 
-with open('offender.txt', 'w') as outfile:
-    json.dump(offender_dict, outfile)
+with open('offender-2.txt', 'w') as outfile:
+    json.dump(offender_dict, outfile, indent=4)
 
 
 
